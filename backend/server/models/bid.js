0bd324529afdb89f9edc5b0ec908cb732d510f4f
Pham Vu Minh <vuminhpham2001@gmail.com>
2022-04-23 02:04:25 +0700

    create bid schema

---
+++ backend/server/models/bid.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const bidSchema = new mongoose.Schema({
+    auctionID: {
+        type: String,
+    },
+    volume: {
+        type: Number,
+    },
+    unit: {
+        type: String,
+    },
+}, {timestamps: true} );
+
+module.exports = mongoose.model("bid", bidSchema);
